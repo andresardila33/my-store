1f199e0cfb29a806410987b82d4d416ebf0c95f7
Andres Ardila <andresardila33@gmail.com>
2022-10-03 11:21:57 -0500

    Manejando parametros tipo query 'GET:parametros query'

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -11,16 +12,21 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-    res.json([
-        {
-            name: 'Product1',
-            price: 1000
-        },
-        {
-            name: 'Product2',
-            price: 2000
-        }
-    ]);
+    const products = [];
+    const { size } = req.query;
+    const limit = size || 10;
+    for (let index = 0; index < limit; index++) {
+        products.push({
+            name: faker.commerce.productName(),
+            price: parseInt(faker.commerce.price(), 10),
+            image: faker.image.imageUrl(),
+        });
+    }
+    res.json(products);
+});
+
+app.get('/products/filter', (req, res) => {
+    res.send('Yo soy un filtro');
 });
 
 app.get('/products/:id', (req, res) => {
@@ -30,6 +36,18 @@ app.get('/products/:id', (req, res) => {
         name: 'Product2',
         price: 2000
     });
+});
+
+app.get('/users', (req, res) => {
+    const { limit, offset } = req.query;
+    if(limit && offset) {
+        res.json({
+            limit,
+            offset
+        });
+    } else {
+        res.send('No hay parametros');
+    }
 });
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
