80cd3962a83aa9e88d50cc84b34dd76e0a9b30a5
Andres Ardila <andresardila33@gmail.com>
2022-10-03 19:07:42 -0500

    Metodo 'Post: metodo para crear', parte 1

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
+const express = require('express');
+
 const productsRouter = require('./products.router');
-const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
+const usersRouter = require('./users.router');
+
 
 function routerApi(app) {
+    const router = express.Router();
+    app.use(/api/v1, router);
     app.use('/products', productsRouter);
-    app.use('/users', usersRouter);
     app.use('/categories', categoriesRouter);
+    app.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
