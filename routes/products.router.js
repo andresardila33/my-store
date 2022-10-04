c32c2adf14b44b648f893bcdfb264ddb2ce0d5fd
Andres Ardila <andresardila33@gmail.com>
2022-10-04 11:50:40 -0500

    Introducción a servicios:creando el primer servicio; 'Crear, editar y eliminar'

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,18 +1,10 @@
 const express = require('express');
-const faker = require('faker');
+const ProductService = require('./../services/product.service');
 const router = express.Router();
+const service = new ProductService();
 
 router.get('/', (req, res) => {
-    const products = [];
-    const { size } = req.query;
-    const limit = size || 10;
-    for (let index = 0; index < limit; index++) {
-        products.push({
-            name: faker.commerce.productName(),
-            price: parseInt(faker.commerce.price(), 10),
-            image: faker.image.imageUrl(),
-        });
-    }
+    const products = service.find();    
     res.json(products);
 });
 
@@ -22,43 +14,27 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
     const { id } = req.params;
-    if (id === '999') {
-        res.status(404).json({
-            message: 'not found'
-        });
-    } else {
-        res.status(200).json({
-            id,
-            name: 'Product2',
-            price: 2000
-        });
-    };
+    const product = service.findOne(id);
+    res.json(product);    
 });
 
 router.post('/', (req, res) => {
     const body = req.body;
-    res.status(201).json({
-        message: 'create',
-        data: body
-    });
+    const newProduct = service.create(body);
+    res.status(201).json(newProduct);
 });
 
 router.patch('/:id', (req, res) => {
     const { id } = req.params;
     const body = req.body;
-    res.json({
-        message: 'update',
-        data: body,
-        id,
-    });
+    const product = service.update(id, body);
+    res.json(product);
 });
 
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
-    res.json({
-        message: 'delete',
-        id,
-    });
+    const rta = service.delete(id);
+    res.json(rta);
 });
 
 module.exports = router;
