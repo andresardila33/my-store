183a4903033af528446af20df6f94568a43b1347
Andres Ardila <andresardila33@gmail.com>
2022-10-04 20:06:59 -0500

    Analizando y solucionando problemas de CORS

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
-const app = express();
-const port = 3000;
+
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
+const app = express();
+const port = 3000;
+
 app.use(express.json());
+const whitelist = ['http://localhost:8080, https://myapp.com'];
+const options = {
+    origin: (origin, cb) => {
+        if (whitelist.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(newError('no permitido'));
+        }
+    }
+};
+
+app.use(cors());
 
 routerApi(app);
 
